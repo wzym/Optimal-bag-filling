3da663c045df58138e94fe6ffd62d0e35dae9ea7
wzym@yandex.ru <Afdqqq*___92>
2019-10-08 18:51:04 +0300

    It works faster without recursion.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
 import java.io.BufferedReader;
-//import java.io.FileReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+//import java.io.InputStreamReader;
 
 public class Main {
-    private Map<Integer, Integer> values = new HashMap<>();
     private int[] gold;
 
     public static void main(String[] args) throws IOException {
-        //long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         new Main().run();
-        //long finishTime = System.currentTimeMillis();
-        //System.out.println(finishTime - startTime);
+        long finishTime = System.currentTimeMillis();
+        System.out.println(finishTime - startTime);
     }
 
     private void run() throws IOException {
@@ -24,23 +21,30 @@ public class Main {
         for (int i = 0; i < partsAmount; i++) {
             gold[i] = Integer.parseInt(params[1][i]);
         }
-        System.out.println(getMaxValue(capacity, gold.length - 1));
+        System.out.println(getMaxValue(capacity));
     }
 
-    private int getMaxValue(int capacity, int lastItem) {
-        int curValue = values.getOrDefault(capacity, 0);
-        for (int i = 0; i <= lastItem; i++) {
-            if (gold[i] <= capacity) {
-                curValue = Math.max(curValue, getMaxValue(capacity - gold[i], i - 1) + gold[i]);
-            }
-            values.put(capacity, curValue);
+    private int getMaxValue(int capacity) {
+        int[][] values = new int[capacity + 1][gold.length + 1];
+        for (int w = 0; w < capacity; w++) {
+            values[w][0] = 0;
         }
-        return curValue;
+        for (int i = 0; i < gold.length; i++) {
+            values[0][i] = 0;
+        }
+        for (int i = 1; i <= gold.length; i++) {
+            for (int w = 1; w <= capacity; w++) {
+                values[w][i] = values[w][i - 1];
+                if (gold[i - 1] <= w)
+                    values[w][i] = Math.max(values[w][i], values[w - gold[i - 1]][i - 1] + gold[i - 1]);
+            }
+        }
+        return values[capacity][gold.length];
     }
 
     private String[][] getParams() throws IOException {
-        //BufferedReader bf = new BufferedReader(new FileReader("Test.txt"));
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader("Test.txt"));
+        //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[][] parameters = new String[2][];
         parameters[0] = bf.readLine().split(" ");
         parameters[1] = bf.readLine().split(" ");
